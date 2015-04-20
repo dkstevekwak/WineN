1b7068069b42b1756b30aae2da714d1d392a4f7c
DJ Nadgar <dj@exciteresearch.com>
2015-04-20 19:08:38 -0400

    Completed product-test.js for the product model with validations and without createdBy, methods, statics, and virtuals

--- tests/server/models/product-test.js
+++ tests/server/models/product-test.js
@@ -23,26 +23,110 @@ describe('Product model', function(){
         expect(Product).to.be.a('function');
     });
 
-    it('should have name, image, description, price, createdBy',function(done){
+    it('should have name, image, description, price, createdBy be strings',function(done){
     	var product = new Product({
-    		name: '', 
-    		image:'', 
-    		description:'', 
-    		price:'', 
-    		createdBy:''
+    		name: "Jimmy's Brew", 
+    		image:'/images/jimmysbrew.png', 
+    		description:"It's organic", 
+    		price:'29.99'
     	});
+        product.save(function(err){
+            product.name.should.equal("Jimmy's Brew");
+            product.image.should.equal('/images/jimmysbrew.png');
+            product.description.should.equal("It's organic");
+            product.price.should.equal('29.99');
+            done(); 
+        });
     });    
 
-    it('should have qty',function(done){
+    it('should have qty which is a Number',function(done){
     	var product = new Product({
-    		qty:[]
+    		qty: 15
     	});
+        product.save(function(err){
+            product.qty.should.equal(15);
+            done(); 
+        });
     });
 
-    it('should have categories',function(done){
-    	var product = new Product({
-    		categories: []
-    	});
+    it('should have categories which is an Array ',function(done){
+        var product = new Product({
+            categories: ['Organic','Red']
+        });
+       product.save(function(err){
+            product.categories.should.equal(['Organic','Red']);
+            done(); 
+        });
+     });
+
+    it('should have createdBy which is an Object reference to a user',function(done){
+        var product = new Product({ 
+            createdBy: {}
+        });
+       product.save(function(err){
+            product.createdBy.should.equal({}); //FIX
+            done(); 
+        });
+    });
+
+    it('should have validation to require name',function(done){
+        var product = new Product({ 
+            image:'/images/jimmysbrew.png', 
+            description:"It's organic", 
+            price:'29.99',
+            qty: 15,
+            categories: ['Organic','Red'],
+            createdBy: {}
+        });
+       product.save(function(err){
+            err.message.should.equal("Validation Failed"); //FI
+            done(); 
+        });
+    });
+
+    it('should require price',function(done){
+        var product = new Product({ 
+            name: "Jimmy's Brew", 
+            image:'/images/jimmysbrew.png', 
+            description:"It's organic", 
+            qty: 15,
+            categories: ['Organic','Red'],
+            createdBy: {}
+       });
+       product.save(function(err){
+            err.message.should.equal("Validation Failed"); //FI
+           done(); 
+        });
+    });
+
+    it('should require catergory',function(done){
+        var product = new Product({ 
+            name: "Jimmy's Brew", 
+            image:'/images/jimmysbrew.png', 
+            description:"It's organic", 
+            price:'29.99',
+            qty: 15,
+            createdBy: {}
+        });
+       product.save(function(err){
+             err.message.should.equal("Validation Failed"); //FI
+          done(); 
+        });
+    });
+
+    it('should require createdBy',function(done){
+        var product = new Product({ 
+            name: "Jimmy's Brew", 
+            image:'/images/jimmysbrew.png', 
+            description:"It's organic", 
+            price:'29.99',
+            qty: 15,
+            categories: ['Organic','Red']
+        });
+       product.save(function(err){
+             err.message.should.equal("Validation Failed"); //FI
+            done(); 
+        });
     });
 
 })
