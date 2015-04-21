e27f73ab4a238ae97b515e1ba41ba4c8b453d7e0
Steve Kwak <steveguac@gmail.com>
2015-04-21 13:11:08 -0400

    DJ and Steve front end Angular updates - cart/profile/products/categories

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -2,6 +2,21 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('profile', {
         url: '/profile',
-        templateUrl: 'js/profile/profile.html'
+        templateUrl: 'js/profile/profile.html',
+        controller: 'ProfileController'
     });
+});
+
+
+app.controller("ProfileController", function($scope){
+	$scope.user = {
+		username: "DJ",
+		password: "pass",
+		email: "email@gmail.com",
+		role: "admin",
+		orders: ['order1','order2'],
+		mailingAddress: "334 79th St",	
+		shippingAddress: "334 79th St"
+	}
+
 });
