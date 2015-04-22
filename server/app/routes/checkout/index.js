1bcba32dfaa953ea0c10636a4378c9eb3615c4f5
DJ Nadgar <dj@exciteresearch.com>
2015-04-22 18:52:03 -0400

    Alex and DJ, created routes for orders server/app/routes/checkout/index.js

--- server/app/routes/checkout/index.js
+++ server/app/routes/checkout/index.js
@@ -1,48 +1,69 @@
 'use strict';
 var router = require('express').Router();
 var Order = require('mongoose').model('Order');
+var _ = require("lodash");
 module.exports = router;
 
-/* Alex and DJ, starting to work on server/routes/checkout/index.js checkoutRoutes-#51
-router.get('/', function(req, res, next) {
-	Product.find({}, function(err, products) {
+/* Alex and DJ, starting to work on server/routes/checkout/index.js checkoutRoutes-#51*/
+//FIX only for Admin -- move to Admin routes
+
+
+//filter by object criteria like by status or date
+
+//front end thank you page with confirmation of order
+
+//guest order
+router.get('/:orderId', function(req, res, next) {
+	//authenticate that order has the right user AT A LATER TI
+	Order.findById({ req.params.orderId }, function(err, order) {
 		if (err) return next(err);
-		res.send(products);
+		res.send(order);
 	});
 });
 
-router.get('/:productId', function(req, res, next) {
-	Product.findById(req.params.productId, function(err, product) {
-		if (err) return next(err);
-		res.send(product);
-	});
-});
-
-router.put('/:productId', function(req,res,next){
+//guest checkout
+router.post('/checkout', function(req,res,next){
 	var body = req.body;
-	console.log("this is server not test", body)
-	Product.findById(req.params.productId, function(err, product) {
+	var order = new Order(body);
+	order.save(function(err,saved){
+		if(err) return next(err);
+		res.send(saved);
+	});
+});
+
+//authenticated 
+
+//checkout
+router.post('/checkout', function(req,res,next){
+	//authenticate to get userId AT A LATER TI
+	var body = req.body;
+	var order = new Order(body);
+	order.save(function(err,saved){
+		if(err) return next(err);
+		res.send(saved);
+	});
+});
+
+//get all orders
+router.get('/', function(req, res, next) {
+	Order.find({}, function(err, orders) {
 		if (err) return next(err);
-		for (var key in body){
-			product[key] = body[key];
-		}
-		// product.name = body.name;
-		// product.image = body.image;
-		// product.description = body.description;
-		// product.price = body.price;
-		// product.qty = body.qty;
-		product.save(function(err, savedProduct){
-			res.send(savedProduct);
+		res.send(orders);
+	});
+});
+
+//put to change the status of an order
+router.put('/:orderId', function(req,res,next){
+	var body = req.body;
+	Order.findById(req.params.orderId, function(err, order) {
+		if (err) return next(err);
+//		for (var key in body){
+//			order[key] = body[key];
+//		}
+		_.extend(order,body);
+		console.log("_.extended order",order);
+		order.save(function(err, saved){
+			res.send(saved);
 		});
 	});
 });
-
-
-router.post('/add', function(req,res,next){
-	var body = req.body;
-	var newProduct = new Product(body);
-	newProduct.save(function(err,savedProduct){
-		if(err) return next(err);
-		res.send(savedProduct);
-	});
-});*/
