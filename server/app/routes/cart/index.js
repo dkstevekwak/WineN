3828fa90440b1aef4b83a7ab32df35dd872f4bc4
DJ Nadgar <dj@exciteresearch.com>
2015-04-23 09:11:37 -0400

    DJ added some cart routes to cart/index.js and a model to cart.js

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -4,65 +4,27 @@ var Cart = require('mongoose').model('Cart');
 var _ = require("lodash");
 module.exports = router;
 
-//FIX only for Admin -- move to Admin routes
+router.post('/', function (req, res,next) {
+    //Read the incoming body data
+    var body = req.body;
+    //Load (or initialize) the cart session id
+    body.session = req.session._id;
+    console.log("body:",body);
 
-
-//filter by object criteria like by status or date
-
-//front end thank you page with confirmation of order
-
-//guest order
-router.get('/:orderId', function(req, res, next) {
-	//authenticate that order has the right user AT A LATER TI
-	Order.findById(req.params.orderId, function(err, order) {
-		if (err) return next(err);
-		res.send(order);
-	});
-});
-
-//guest checkout
-router.post('/checkout', function(req,res,next){
-	var body = req.body;
-	var order = new Order(body);
-	order.save(function(err,saved){
+	var newCart = new Cart(body);
+	newCart.save(function(err,saved){
 		if(err) return next(err);
 		res.send(saved);
 	});
 });
-
-//authenticated 
-
-//checkout
-router.post('/checkout', function(req,res,next){
-	//authenticate to get userId AT A LATER TI
-	var body = req.body;
-	var order = new Order(body);
-	order.save(function(err,saved){
-		if(err) return next(err);
-		res.send(saved);
-	});
-});
-
-//get all orders
-router.get('/', function(req, res, next) {
-	Order.find({}, function(err, orders) {
+}).get('/:cartId', function(req, res, next) {
+	Cart.findById(req.params.cartId, function(err, cart) {
 		if (err) return next(err);
-		res.send(orders);
+		res.send(cart);
 	});
-});
-
-//put to change the status of an order
-router.put('/:orderId', function(req,res,next){
-	var body = req.body;
-	Order.findById(req.params.orderId, function(err, order) {
+}).put('/:cartId', function(req, res, next) {
+	Cart.findById(req.params.cartId, function(err, cart) {
 		if (err) return next(err);
-//		for (var key in body){
-//			order[key] = body[key];
-//		}
-		_.extend(order,body);
-		console.log("_.extended order",order);
-		order.save(function(err, saved){
-			res.send(saved);
-		});
+		res.send(cart);
 	});
 });
--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,62 +2,18 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-	user: { //either user._id or session._id
-        type: String,
-		required: true
-		//unique : true,
-		//dropDups: true
-    },
-    paid: {
-        type: Boolean,
-        default: false
-    },
+    session: {
+		type: String
+	},
+    user: [{
+		type: mongoose.Schema.Types.ObjectId, ref: 'User'
+	}],
     cartProducts: {
-        type: [],
-    	default: [] //we don't have to check if it exists or not
-    },
-    status: {
-        type: String,
-        required: true
+        type: []
     },
     date: {
 		type: Date,
-		default: new Date()
-    },
-    promoCode: {
-        type: String
-    },
-	shipping: {
-		type: String
-	},
-    tax: {
-		type: String
-	},
-    subTotal: {
-		type: String
-	},
-    total: {
-		type: String
-	},
-	firstName: {
-		type: String
-	},
-	lastName: {
-		type: String
-	},
-	shippingAddress: {
-		line1: String,
-		line2: String,
-		city: String,
-		state: String,
-		zip: String
-	},
-	billingAddress: {
-		line1: String,
-		line2: String,
-		city: String,
-		state: String,
-		zip: String
-	},
+		default: Date.now
+    }
 });
 mongoose.model('Cart', schema);
