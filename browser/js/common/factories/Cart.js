a40bce74fce066d2c6f0dfddebe6fe7113ad53e2
Alexander Wang <alwang85@Alexanders-MacBook-Pro.local>
2015-04-24 12:49:08 -0400

    tentatively finished cart factory to sync cloud

--- browser/js/common/factories/Cart.js
+++ browser/js/common/factories/Cart.js
@@ -1,5 +1,5 @@
 'use strict';
-app.factory('Cart', function ($http,localStorageService) {
+app.factory('Cart', function ($http,localStorageService, Users) {
 	// nice to haves cartID, total numberOfItems and subtotal for cookie/session
 
 	var shipping = 5;
@@ -27,6 +27,7 @@ app.factory('Cart', function ($http,localStorageService) {
 		if (productExists(product._id)){
 			incrementQty(product._id);
 			localStorage.setItem('cart',JSON.stringify(localCart));
+
 			return;
 		}
 
@@ -36,11 +37,14 @@ app.factory('Cart', function ($http,localStorageService) {
 		localCart.push(product);
 		localStorage.setItem('cart',JSON.stringify(localCart));
 		//get local storage in the browser
-		$http.get('api/cart', {
-            params: { productId: product._id }
-        }).then(function (response) {
-            return response.data;
-        });
+
+		cloudCartSync();
+		//assuming
+		//$http.get('api/cart', {
+     //       params: { productId: product._id }
+     //   }).then(function (response) {
+     //       return response.data;
+		//});
 	};
 	//empty cart
 	var emptyCart = function(){
@@ -53,7 +57,51 @@ app.factory('Cart', function ($http,localStorageService) {
 //			cart.pop();
 //		}
 	};
+	var readCart = function(){
+		localCart = JSON.parse(localStorage.getItem('cart'));
+	};
 
+	var cloudCartSync = function(){
+		readCart();
+		Users.getCurrentUser().then(function(user){
+			if (localCart.length == 0 && user.cart.length > 0) {
+				localStorage.setItem('cart',JSON.stringify(user.cart));
+				readCart();
+			}
+			else if (localCart.length !== 0) {
+				updateCloudCart();
+			}
+			console.log('cloudCartSynced');
+		}, function(err){
+			console.log('failed to get current user', err);
+			return null;
+		})
+
+
+	};
+	cloudCartSync(); //checks cloud cart on load;
+
+	var getCloudCart = function(){
+		console.log('getting cloud cart');
+		return $http.get('/api/' + user._id + '/cart').then(function(res){
+			console.log('got serverCart', res);
+			return res.data; //should be promise object that is a cart;
+		}, function(err){
+			console.log('failed to update cart', err)
+		})
+	};
+	var updateCloudCart = function(){
+		console.log('serverCart Updating');
+		return $http.put('/api/' + user._id + '/cart', localCart).then(function(res){
+			console.log('serverCart Updated', res.data);
+			return res.data; //promise object that should be a cart;
+		}, function(err){
+			console.log('failed to update cart', err)
+		})
+	};
+
+
+	//cart is  cleared server side upon order creation;
 	var removeItem = function(product){
 		//local storage
 		localCart = JSON.parse(localStorage.getItem('cart'));
