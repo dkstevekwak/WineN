c318912718fb46097f4904c48cdf80d52c2feae7
DJ Nadgar <dj@exciteresearch.com>
2015-04-25 15:04:31 -0400

    Jimmy and DJ rocked out search on the product page using angular live filtering. BAMgit add -A!

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -21,9 +21,23 @@ app.controller('ProductsController', function($scope, Products, Cart, localStora
     console.log("errors", err);
   })
   
+  	//Add to Cart
 	$scope.addToCart = function(product) {
 		console.log("add to cart")
 		Cart.addToCart(product);
+	};
+	
+	//Search
+	$scope.search = function (product){
+		//query box is empty
+		if(!$scope.query) return true;
+		//query box has a string
+		var categoriesStr = product.categories.join(",");
+		if (product.name.indexOf($scope.query)!=-1 
+				|| categoriesStr.indexOf($scope.query)!=-1) {
+		        return true;
+		    }
+		return false;
 	};
 
 });
