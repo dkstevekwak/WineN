16f24c04cadf59751ffaf3255d8e01b34e2f33d9
Alex Wang <alwang85@gmail.com>
2015-04-28 21:12:46 -0400

    test commit for rec engine

--- browser/js/common/factories/Recommendations.js
+++ browser/js/common/factories/Recommendations.js
@@ -1,7 +1,7 @@
 'use strict';
 app.factory('Recommendations', function ($http,localStorageService, Users, AuthService) {
 	var getAllRecs = function(){
-	    return $http.get('http://192.168.1.216:1338/api/').then(function(res){
+	    return $http.get('https://winen.herokuapp.com:23783/api/').then(function(res){
 	    	console.log("getAllRecs res.data",res.data);
 	    	return res.data;
 	    }, function(err){
@@ -9,7 +9,7 @@ app.factory('Recommendations', function ($http,localStorageService, Users, AuthS
 	    });
 	  };
 	var getProductRec = function(pid){
-		var pidRequestStr = 'https://winen.herokuapp.com:444/api/'+pid;
+		var pidRequestStr = 'https://winen.herokuapp.com:23783/api/'+pid;
 	    return $http.get(pidRequestStr.toString()).then(function(res){
 	    	var arr = [];
 	    	for (var key in res.data){
